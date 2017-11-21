c8ffe5ac5ecc40552c9b8b6bfd661662c6605c76
Sajid <sajed1990@gmail.com>
2017-11-21 15:42:42 +0600

    Crawler

--- src/main/groovy/startech/crawler/MultiMediaKingdom.groovy
+++ src/main/groovy/startech/crawler/MultiMediaKingdom.groovy
@@ -1,9 +1,9 @@
 package startech.crawler
 
-import org.jsoup.Jsoup;
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import thread.pool.MyMonitorThread
 import util.DB
 
@@ -11,16 +11,15 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-
-public class JSoupTest {
+public class MultiMediaKingdom {
     static db = new DB("price_comapre");
     static ConcurrentHashMap<String, Integer> failedCount = new ConcurrentHashMap<String, Integer> ()
 
     static List getAllProductURLs(Document categoryDoc) {
         List<String> productURLs = []
-        Elements products = categoryDoc.select(".products-grid > li");
+        Elements products = categoryDoc.select(".product_item_wrapper");
         for (Element product : products) {
-            Element name = product.select(".product-name a")[0]
+            Element name = product.select(".product-title a")[0]
             if(name) {
                 productURLs.add(name.attr("href"))
             }
@@ -30,31 +29,20 @@ public class JSoupTest {
 
     static crawlProduct(String productUrl) {
         Document productDoc = Jsoup.connect(productUrl).get();
-        Elements specs = productDoc.select("#product-attribute-specs-table tr");
-        String name = productDoc.select(".product-name h2")[0]?.text()?.trim()
-        String code = productDoc.select(".productsku span")[0]?.text()?.trim()
-        String price = productDoc.select(".regular-price .price")[0]?.text()?.trim()
-        price = price ?: productDoc.select(".special-price .price")[0]?.text()?.trim()
+        String name = productDoc.select(".product-title")[0]?.text()?.trim()
+        String code = productDoc.select("[itemscope].product").attr("id")?.trim()
+        String price = productDoc.select(".price .amount")[0]?.text()?.trim()
         if(price) {
-            price = price.replaceAll("[A-Za-z,]", "")
+            price = price.replace('৳', "").replaceAll("[A-Za-z,]", "").trim()
         }
-        String model = ""
-        Iterator<Element> iter = specs.iterator()
-        while (iter.hasNext()) {
-            Element spec = iter.next();
-            String label = spec.select(".label")[0]?.text()?.trim()
-            String value = spec.select(".data")[0]?.text()?.trim()
-            if(label == "Model") {
-                model = value
-                break
-            }
-        }
-        Integer result = db.insert("INSERT INTO `ryans_product` (`name`, `code`, `model`, `url`, `price`) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE `name` = ?, `model` = ?, `url` = ?, `price` = ?, `updated` = now()", [name, code, model, productUrl, price, name, model, productUrl, price])
-        if(result) {
+        String model = productDoc.select(".wd_product_sku")[0]?.text()?.trim() ?: ""
+        model = model.replace("Sku: ", "")
+        Integer result = db.insert("INSERT INTO `mk_product` (`name`, `code`, `model`, `url`, `price`) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE `name` = ?, `model` = ?, `url` = ?, `price` = ?, `updated` = now()", [name, code, model, productUrl, price, name, model, productUrl, price])
+//        if(result) {
 //            println("Product save succes: $code")
-        } else {
+//        } else {
 //            println("Product save failed: $code")
-        }
+//        }
     }
 
     static void crawlCategory(String categoryURL) {
@@ -63,7 +51,7 @@ public class JSoupTest {
         Document doc = Jsoup.connect(categoryURL).get();
         while (doc) {
             productURLs.addAll(getAllProductURLs(doc))
-            Element nextPage = doc.select(".pages .next.i-next")[0]
+            Element nextPage = doc.select(".next.page-numbers")[0]
             String nextPageURL = nextPage ? nextPage.attr("href") : null
             doc = nextPageURL ? Jsoup.connect(nextPageURL).get() : null
         }
@@ -77,39 +65,11 @@ public class JSoupTest {
     }
 
 
-    static void crawler() {
-        Document doc = Jsoup.connect("https://ryanscomputers.com/").get();
-        Elements menus = doc.select("ul.sm_megamenu_menu > li.other-toggle")
-        menus.remove(0)
-        List<String> categoryURLs = []
-        menus.each {
-            it.select("a").each {
-                String url = it.attr("href").trim()
-                if (url.startsWith("http")) {
-                    categoryURLs.add( url)
-                }
-            }
-        }
-
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-        MyMonitorThread monitor = new MyMonitorThread(executor, 3);
-        Thread monitorThread = new Thread(monitor);
-        monitorThread.start();
-        categoryURLs.each { url ->
-            executor.execute({
-                try {
-                    crawlCategory(url)
-                } catch (Exception ex) {
-                    println("Category URL: "  + url + "\nMessage: "  + ex.message + "\n----------------------------------------------")
-                }
-            })
-        }
-        executor.shutdown()
-    }
 
     public static void CrawlCategories() {
         List categoryURLs = [
-                "https://ryanscomputers.com/office-equipment/projector.html",
+                "http://www.startech.com.bd/accessories/webcam/logitech-webcam",
+                "https://ryanscomputers.com/accessories/gaming.html"
         ]
         ExecutorService executor = Executors.newFixedThreadPool(5);
         MyMonitorThread monitor = new MyMonitorThread(executor, 3);
@@ -127,7 +87,46 @@ public class JSoupTest {
         executor.shutdown()
     }
 
+    static void crawler() {
+        Document doc = Jsoup.connect("http://multimediakingdom.com.bd/").get();
+        Elements menus = doc.select("#menu-main li")
+        menus.remove(0)
+        List<String> categoryURLs = []
+        menus.each {
+            it.select("a").each {
+                String url = it.attr("href").trim()
+                if (url.startsWith("http")) {
+                    categoryURLs.add( url)
+                }
+            }
+        }
+
+        menus = doc.select("ul.hover_mode li")
+        menus.each {
+            it.select("a").each {
+                String url = it.attr("href").trim()
+                if (url.startsWith("http")) {
+                    categoryURLs.add( url)
+                }
+            }
+        }
+        ExecutorService executor = Executors.newFixedThreadPool(15);
+        MyMonitorThread monitor = new MyMonitorThread(executor, 3);
+        Thread monitorThread = new Thread(monitor);
+        monitorThread.start();
+        categoryURLs.each { url ->
+            executor.execute({
+                try {
+                    crawlCategory(url)
+                } catch (Exception ex) {
+                    println("Category URL: "  + url + "\nMessage: "  + ex.message + "\n----------------------------------------------")
+                }
+            })
+        }
+        executor.shutdown()
+    }
+
     public static void main(String[] args) {
-        CrawlCategories()
+        crawler()
     }
 }
